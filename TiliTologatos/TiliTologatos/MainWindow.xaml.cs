6d8c7927549b108ad27981955030d28b7d8b27f6
Ujj Ádám <adamujj66@gmail.com>
2020-12-15 16:44:08 +0100

    Pozíció
    Tömbben az elfoglalt pozíció

--- TiliTologatos/TiliTologatos/MainWindow.xaml.cs
+++ TiliTologatos/TiliTologatos/MainWindow.xaml.cs
@@ -26,5 +26,18 @@ namespace TiliTologatos
         }
         int[] helyzet = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
         int[] kesz = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Button ezGomb = sender as Button;
+            Button nullaGomb = (Button)FindName("Button0");
+
+            var fTav = Math.Abs(ezGomb.Margin.Top - nullaGomb.Margin.Top);
+            var vTav = Math.Abs(ezGomb.Margin.Left - nullaGomb.Margin.Left);
+
+            int ezGombFelirat = int.Parse(ezGomb.Content.ToString());
+            int ezGombIndex = Array.IndexOf(helyzet, ezGombFelirat);
+            int nullaGombIndex = Array.IndexOf(helyzet, 0);
+        }
     }
 }
